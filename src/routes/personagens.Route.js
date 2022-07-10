3609def7bf8008593408283f6f39055e9c4a6727
LucasSSDK <l_ucass_oares@outlook.com>
2022-07-10 18:24:56 -0300

    atualizacao

--- src/routes/personagens.Route.js
+++ src/routes/personagens.Route.js
@@ -3,3 +3,17 @@ const express = require('express');
 const controller = require('../controllers/controller');
 
 const route = require('express').Router();
+
+route.get('/allPersonagens', controller.findAllPersonController);
+
+route.get('/personagens/:id', controller.findByIdPersonController);
+
+route.post('/createPersonagem', controller.createPerson);
+
+route.put('/updatePersonagem/:id',
+controller.updatePerson);
+
+route.delete('/deletePersonagem/:id',
+controller.deletePerson);
+
+module.exports = route;
